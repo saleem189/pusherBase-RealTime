3981ed63cff75cc7bcfa6ef627c3b28c00b41a0c
Saleem Ayoub Khan <75361545+saleem189@users.noreply.github.com>
2022-05-30 09:55:47 +0500

    Puser RealTime data

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="card">
+        <div class="card-header">Chats</div>
+        <div class="card-body">
+            <chat-messages :messages="messages"></chat-messages>
+        </div>
+        <div class="card-footer">
+            <chat-form v-on:messagesent="addMessage" :user="{{ Auth::user() }}"></chat-form>
+        </div>
+        <span  v-show="typing" class="help-block" style="font-style: italic;">
+            @{{ user }} is typing...
+</span>
+    </div>
+</div>
+@endsection
